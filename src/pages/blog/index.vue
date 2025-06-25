e26ee80e7524aa2d54bf3ff824a0e992ba57a014
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2025-06-25 05:10:44 +0200

    fix: responsive blog

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -8,14 +8,35 @@ function formatDate(date: string): string {
 </script>
 
 <template>
-  <section class="flex gap-8 prose dark:prose-invert" aria-labelledby="section-title-unique">
-    <h1 id="section-title-unique" class="sr-only">Florian Beaumont – Technical Blog & Vue.js Learning</h1>
-    <h2 class="hidden xl:block pb-8 mt-0 text-8xl font-bold">B<br>L<br>O<br>G</h2>
-    <ul class="px-6 mt-12 xl:min-w-4xl">
-      <li class="list-disc py-2 text-xl underline-offset-6" v-for="post in posts" :key="post.slug">
-        <router-link :to="`/blog/${post.slug}`" class="flex justify-between gap-4">
-          {{ post.frontmatter.title }}<br>
-          <span class="hidden xl:block text-xs bg-white text-black px-2.5 py-1.5 rounded no-underline">{{ formatDate(post.frontmatter.date) }}</span>
+  <section
+    class="flex flex-col lg:flex-row xl:gap-12 items-start xl:items-stretch px-4 sm:px-8 md:px-12 py-10 max-w-screen-xl mx-auto"
+    aria-labelledby="section-title-unique"
+  >
+    <h1 id="section-title-unique" class="sr-only">
+      Florian Beaumont – Technical Blog &amp; Vue.js Learning
+    </h1>
+
+    <h2 class="hidden lg:flex flex-col justify-start items-center mr-8 mt-0 pb-8 text-8xl font-bold leading-none select-none">
+      <span>B</span>
+      <span>L</span>
+      <span>O</span>
+      <span>G</span>
+    </h2>
+
+    <ul class="w-full max-w-2xl prose prose-li:mb-0 prose-li:mt-0 dark:prose-invert xl:mt-0 mt-6">
+      <li
+        v-for="post in posts"
+        :key="post.slug"
+        class="py-3 text-lg md:text-xl transition-colors hover:bg-gray-50 dark:hover:bg-gray-900 rounded"
+      >
+        <router-link
+          :to="`/blog/${post.slug}`"
+          class="flex flex-col xl:flex-row xl:items-center justify-between gap-2 xl:gap-4 underline underline-offset-4"
+        >
+          <span class="font-medium break-words">{{ post.frontmatter.title }}</span>
+          <span class="text-xs bg-white text-black px-2.5 py-1.5 rounded self-start xl:self-auto mt-1 xl:mt-0">
+            {{ formatDate(post.frontmatter.date) }}
+          </span>
         </router-link>
       </li>
     </ul>
